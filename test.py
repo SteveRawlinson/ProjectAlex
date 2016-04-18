d61237d40127eccbede3fe9431899995a1900b0c
Steve Rawlinson <steve@tagadab.com>
2016-04-18 19:21:58 +0100

    bugs

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ import time
 class MyTest(jmri.jmrit.automat.AbstractAutomaton):
 
     def init(self):
-        t = self.getThrottle(2144, True)
+        self.t = self.getThrottle(2144, True)
 
     def handle:
         print "speed 0.2"
-        t.setSpeed(0.2)
+        self.t.setSpeed(0.2)
         time.sleep(3)
         print "speed -1"
-        t.setSpeed(-1)
+        self.t.setSpeed(-1)
         time.sleep(3)
         print "speed 0"
+        self.t.setSpeed(0)
 
 MyTest().start()
