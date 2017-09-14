fbcb36ae476e748e357d1a251152778133f6f153
Steve Rawlinson <steve@tagadab.com>
2017-09-14 18:18:09 +0100

    new route class

---
+++ classFastNth2SthTrack5Nonstop.py
@@ -0,0 +1,69 @@
+import jmri
+import time
+import sys
+sys.path.append('C:\\Users\\steve\\JMRI\\jython')
+import alex
+import loco
+from jmri_bindings import *
+from myroutes import *
+
+class ClassFastNth2SthTrack5Nonstop(alex.Alex):
+
+    def __init__(self, loc, memory):
+        self.loco = loc
+        self.knownLocation = None
+        self.memory = memory
+
+    def handle(self):
+        if not self.loco.northSidings():
+            raise RuntimeError("I'm not in the north sidings")
+
+        self.loco.status = loco.MOVING
+        start = time.time()
+
+        # get a 'lock' on the north link track
+        lock = self.getLock('North Link Lock')
+
+        # Out the nth sidings
+        routes = self.requiredRoutes(self.loco.block) + self.requiredRoutes("Nth Fast Inner 1")
+        self.shortJourney(True, self.loco.block, "Nth Fast Link", 0.4, routes=routes, lock=lock)
+
+        # slower round the bend
+        self.shortJourney(True, self.loco.block, "Nth Fast Inner 1", 0.2)
+
+        # off to the other side of the layout
+        self.shortJourney(True, self.loco.block, "Sth Fast Inner 2", 0.5)
+
+        # get a lock on the south link, but if it's not available immediately we need to know pronto
+        lock = self.getLockNonBlocking('South Link Lock')
+        if lock is False:
+            # stop the train at FPK 7
+            self.shortJourney(True, self.loco.block, "FPK P7", 0.3, 0.1, 1000)
+            # wait for a lock
+            lock = self.getLock('South Link Lock')
+        else:
+            # we got the lock - set the turnouts for FPK 7
+            for r in self.requiredRoutes("FPK P7"):
+                self.setRoute(r, 0)
+            # progress to FPK 7
+            self.shortJourney(True, self.loco.block, "FPK P7", 0.4)
+            # check we still have the lock
+            rc = self.checkLock(lock)
+            if rc is False :
+                # we lost it, abort!
+                self.throttle.setSpeedSetting(0)
+                print loco, "race conditions on South Link Lock?"
+                return False
+
+        # select a siding
+        siding = self.loco.selectSiding(SOUTH_SIDINGS)
+        routes = self.requiredRoutes(siding)
+        self.shortJourney(True, self.loco.block, siding, 0.3, 0.2, 0, stopIRClear=IRSENSORS[siding.getID()], routes=routes, lock=lock)
+        self.unlock('South Link Lock')
+
+        print "route complete."
+        stop = time.time()
+        print "route took", stop - start, 'seconds'
+        self.loco.status = loco.SIDINGS
+
+        return False
