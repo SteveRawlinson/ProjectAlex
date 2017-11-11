39fba2b2e0fe093eff6a327ee078e05d43435205
Steve Rawlinson <steve@tagadab.com>
2017-11-11 20:05:58 +0000

    note jack status if we stopped early

--- class150Sth2NthTrack2Stopping.py
+++ class150Sth2NthTrack2Stopping.py
@@ -72,6 +72,8 @@ class Class150Sth2NthTrack2Stopping(alex.Alex):
                 siding = self.loco.selectSiding(NORTH_SIDINGS)
                 routes = self.requiredRoutes(siding)
                 self.shortJourney(False, self.loco.block, siding, fast, stopIRClear=IRSENSORS[siding.getId()], routes=routes, lock=lock)
+            else:
+                self.debug("jack status: " + str(self.getJackStatus()))
         else:
             # PAL to North sidings
             siding = self.loco.selectSiding(NORTH_SIDINGS)
