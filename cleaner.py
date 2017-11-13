aed03e55bd10b8ef727cba83d53361b6452f2d65
Steve Rawlinson <steve@tagadab.com>
2017-11-13 17:33:41 +0000

    add northSidings()

--- cleaner.py
+++ cleaner.py
@@ -30,68 +30,10 @@ class Cleaner(alex.Alex):
             self.tracks.append(tr)
             print "New track: array index: " + str(self.tracks.index(tr)) + " track nr: " + str(tr.nr) + " stops: " + str(tr.stops) + " fast: " + str(tr.fast)
 
-    def handle(self):
-
-        # get track info
-        self.initTracks()
-
-        # turn layout power on
-        self.powerState = powermanager.getPower()
-        if self.powerState != jmri.PowerManager.ON:
-            poweredOn = True
-            self.debug("turning power on")
-            powermanager.setPower(jmri.PowerManager.ON)
-        else:
-            poweredOn = False
-            self.debug("power is on")
-
-        # get a throttle
-        self.getLocoThrottle(self.loco)
-        self.debug("got throttle")
-
-        if poweredOn:
-            # stop the loco in case it started up
-            self.loco.emergencyStop()
-            self.debug("sleeping for 5 ...")
-            time.sleep(5)
-
-        # get a block if we don't have one
-        self.debug("sorting out blocks")
-        if self.loco.block is None:
-            # put up a dropbox for the user to select the block
-            blist = ['not in use']
-            blockNameList = []
-            for t in self.tracks:
-                blockNameList += t.blocks
-            for blockName in blockNameList:
-                blk = blocks.getBlock(blockName)
-                if blk.getState () == OCCUPIED:
-                    if str(blk.getValue()) == str(self.loco.dccAddr):
-                        self.debug("found loco in block " + blockName)
-                        self.loco.setBlock(blk)
-                    elif blk.getValue() is None or blk.getValue() == "":
-                        blist.append(blockName)
-            if self.loco.block is None:
-                self.debug("getting block from user")
-                b = JOptionPane.showInputDialog(None,
-                                            "Select starting block for " + self.loco.name(),
-                                            "Choose block",
-                                            JOptionPane.QUESTION_MESSAGE,
-                                            None,
-                                            blist,
-                                            'not in use')
-                if b is None:
-                    # User cancelled
-                    return False
-                if b != "not in use":
-                    # set the block and add the new loco to the list
-                    self.loco.setBlock(b)
-
-        if self.loco.block is None:
-            raise RuntimeError("I don't have a block!")
+    # Cleans a pair of tracks, eg. tracks 1 and 2
+    def trackPair(self):
 
         startBlock = self.loco.block
-
         self.loco.status = loco.MOVING
 
         # we will shortly start moving but we don't know which
@@ -238,6 +180,113 @@ class Cleaner(alex.Alex):
             self.waitChange([s])
         self.waitChange([s])
         self.loco.setSpeedSetting(-1)
+
+    def northSidings(self):
+        trak = track.Track.findTrackByBlock(self.tracks, self.loco.block)
+        self.debug("track: " + str(trak.nr))
+
+        startBlock = self.loco.block()
+
+        # set exit route for this track
+        route = trak.exitRoute()
+        self.setRoute(route)
+
+        for siding in NORTH_SIDINGS:
+            # set the route into the siding
+            route = self.requiredRoutes(siding)
+            self.setRoute(route)
+            self.loco.forward()
+            self.loco.setSpeedSetting(0.4)
+            # wait for the siding to become occupied
+            lb = layoutblocks.getLayoutBlock(siding)
+            ls = lb.getOccupancySensor()
+            if ls.knownState() == INACTIVE:
+                self.waitChange([ls], 60 * 1000)
+            # wait for a bit to get into the siding
+            time.sleep(CLEANER_SIDING_TIME[siding])
+            # turn loco around
+            self.loco.reverse()
+            # wait till IR sensor goes active
+            ls = sensors.getSensors(IRSENSORS["North Link Clear"])
+            if ls.knownState() == INACTIVE:
+                self.waitChange([ls], 60 * 1000)
+            # and then wait for it to clear
+            self.waitChange([ls], 60 * 1000)
+            self.loco.setSpeedSetting(0)
+
+        # reverse back to the block we started on
+        self.loco.reverse()
+        self.loco.setSpeedSetting(0.4)
+        sen = startBlock.getOccupancySensor()
+        self.waitChange([sen], 120 * 1000)
+        # and just past it
+        self.loco.setSpeedSetting(0.3)
+        self.waitChange([sen], 60 * 1000)
+
+
+
+    def handle(self):
+
+        # get track info
+        self.initTracks()
+
+        # turn layout power on
+        self.powerState = powermanager.getPower()
+        if self.powerState != jmri.PowerManager.ON:
+            poweredOn = True
+            self.debug("turning power on")
+            powermanager.setPower(jmri.PowerManager.ON)
+        else:
+            poweredOn = False
+            self.debug("power is on")
+
+        # get a throttle
+        self.getLocoThrottle(self.loco)
+        self.debug("got throttle")
+
+        if poweredOn:
+            # stop the loco in case it started up
+            self.loco.emergencyStop()
+            self.debug("sleeping for 5 ...")
+            time.sleep(5)
+
+        # get a block if we don't have one
+        self.debug("sorting out blocks")
+        if self.loco.block is None:
+            # put up a dropbox for the user to select the block
+            blist = ['not in use']
+            blockNameList = []
+            for t in self.tracks:
+                blockNameList += t.blocks
+            for blockName in blockNameList:
+                blk = blocks.getBlock(blockName)
+                if blk.getState () == OCCUPIED:
+                    if str(blk.getValue()) == str(self.loco.dccAddr):
+                        self.debug("found loco in block " + blockName)
+                        self.loco.setBlock(blk)
+                    elif blk.getValue() is None or blk.getValue() == "":
+                        blist.append(blockName)
+            if self.loco.block is None:
+                self.debug("getting block from user")
+                b = JOptionPane.showInputDialog(None,
+                                            "Select starting block for " + self.loco.name(),
+                                            "Choose block",
+                                            JOptionPane.QUESTION_MESSAGE,
+                                            None,
+                                            blist,
+                                            'not in use')
+                if b is None:
+                    # User cancelled
+                    return False
+                if b != "not in use":
+                    # set the block and add the new loco to the list
+                    self.loco.setBlock(b)
+
+        if self.loco.block is None:
+            raise RuntimeError("I don't have a block!")
+
+
+        self.trackPair()
 
         self.debug("exiting")
         return False
