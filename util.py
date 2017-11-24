58c050ad6582def26f7a42d21aae576ee85d76d5
Steve Rawlinson <steve@tagadab.com>
2017-11-24 13:39:56 +0000

    less debug
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- util.py
+++ util.py
@@ -17,17 +17,17 @@ class Util:
     def isBlockOccupied(self, thing):
         self.debug("isBlockOccupied: thing type: " + type(thing).__name__ + " value: " + str(thing))
         block, sensor = self.convertToLayoutBlockAndSensor(thing)
-        self.debug("  block: " + block.getDisplayName())
-        if sensor is not None:
-            self.debug("  sensor: " + sensor.getSystemName())
+        #self.debug("  block: " + block.getDisplayName())
+#        if sensor is not None:
+#            self.debug("  sensor: " + sensor.getSystemName())
         if sensor is None:
-            self.debug("  sensor is none")
+#            self.debug("  sensor is none")
             return False
         if sensor.getKnownState() == ACTIVE:
             # see if we know the identity of the loco
             b = block.getBlock()
             if b.getValue() is not None:
-                self.debug("  returning value: " + b.getValue())
+ #               self.debug("  returning value: " + b.getValue())
                 return b.getValue()
             else:
                 return True
