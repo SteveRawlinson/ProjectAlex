a599612dc412c460b59ad528bf066a7468f9653e
Steve Rawlinson <steve@tagadab.com>
2017-11-29 14:12:19 +0000

    use moveIntoNorthSidings()
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- class150Sth2NthTrack4Stopping.py
+++ class150Sth2NthTrack4Stopping.py
@@ -52,27 +52,29 @@ class Class150Sth2NthTrack4Stopping(alex.Alex):
             m = memories.provideMemory(self.memory)
             m.setValue(0)
 
-        if self.getJackStatus() == NORMAL and self.loco.rarity() == 0:
-            # If this loco has a rarity of zero and we're not shutting down operations
-            # there's no point in going all the way to the sidings because we'll just get
-            # started up again. Stop on the North Link
-            self.debug("stopping early")
-            routes = self.requiredRoutes(self.loco.block)
-            self.shortJourney(False, self.loco.block, "North Link", medium, slowSpeed=slow, routes=routes)
-            # check JackStatus hasn't changed in the meantime
-            if self.getJackStatus() == STOPPING:
-                self.debug("JackStatus is now STOPPING - moving to siding")
-                siding = self.loco.selectSiding(NORTH_SIDINGS)
-                routes = self.requiredRoutes(siding)
-                self.shortJourney(False, self.loco.block, siding, fast, stopIRClear=IRSENSORS[siding.getId()], routes=routes, lock=lock)
-        else:
-            # PAL to North sidings
-            self.debug("not stopping early. status :" + str(self.getJackStatus()) + " doesn't equal normal: " + str(NORMAL) + " self.rarity(): " + str(self.loco.rarity()))
-            siding = self.loco.selectSiding(NORTH_SIDINGS)
-            routes = self.requiredRoutes(self.loco.block)
-            self.shortJourney(False, self.loco.block, "North Link", medium, routes=routes, lock=lock)
-            routes = self.requiredRoutes(siding)
-            self.shortJourney(False, self.loco.block, siding, fast, stopIRClear=IRSENSORS[siding.getId()], routes=routes, lock=lock)
+        self.moveIntoNorthSidings()
+
+        # if self.getJackStatus() == NORMAL and self.loco.rarity() == 0:
+        #     # If this loco has a rarity of zero and we're not shutting down operations
+        #     # there's no point in going all the way to the sidings because we'll just get
+        #     # started up again. Stop on the North Link
+        #     self.debug("stopping early")
+        #     routes = self.requiredRoutes(self.loco.block)
+        #     self.shortJourney(False, self.loco.block, "North Link", medium, slowSpeed=slow, routes=routes)
+        #     # check JackStatus hasn't changed in the meantime
+        #     if self.getJackStatus() == STOPPING:
+        #         self.debug("JackStatus is now STOPPING - moving to siding")
+        #         siding = self.loco.selectSiding(NORTH_SIDINGS)
+        #         routes = self.requiredRoutes(siding)
+        #         self.shortJourney(False, self.loco.block, siding, fast, stopIRClear=IRSENSORS[siding.getId()], routes=routes, lock=lock)
+        # else:
+        #     # PAL to North sidings
+        #     self.debug("not stopping early. status :" + str(self.getJackStatus()) + " doesn't equal normal: " + str(NORMAL) + " self.rarity(): " + str(self.loco.rarity()))
+        #     siding = self.loco.selectSiding(NORTH_SIDINGS)
+        #     routes = self.requiredRoutes(self.loco.block)
+        #     self.shortJourney(False, self.loco.block, "North Link", medium, routes=routes, lock=lock)
+        #     routes = self.requiredRoutes(siding)
+        #     self.shortJourney(False, self.loco.block, siding, fast, stopIRClear=IRSENSORS[siding.getId()], routes=routes, lock=lock)
 
         stop = time.time()
         print self.loco.dccAddr, "route completed in", stop - start, 'seconds'
