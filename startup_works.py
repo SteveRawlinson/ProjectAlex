447917fd1e38b26ac6ce74458d88cfe5a4d76b64
Steve Rawlinson <steve@tagadab.com>
2017-12-07 10:53:10 +0000

    remove non-working stuff which causes exceptions opn new jmri version
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- startup_works.py
+++ startup_works.py
@@ -3,57 +3,57 @@ import time
 from myroutes import *
 from jmri_bindings import *
 
-class StartUp :
-
-    def do(self) :
-    
-        self.powerState = powermanager.getPower()
-        if self.powerState != jmri.PowerManager.ON :
-            powermanager.setPower(jmri.PowerManager.ON)
-            time.sleep(1)
-    
-        r = None
-        tries = 0
-        while r == None and tries < 10 :        
-            r = routes.getRoute('StartUp')
-            if r == None:
-                tries += 1
-                print "tries:", tries
-                time.sleep(2)
-
-        r.activateRoute()
-        r.setRoute()
-        time.sleep(2)
-
-        r = routes.getRoute('Hertford Nth Inner')
-        r.activateRoute()
-        r.setRoute()
-        time.sleep(2)
-
-        r = routes.getRoute('Sth Sidings 1')
-        r.activateRoute()
-        r.setRoute()
-        time.sleep(2)
-
-        r = routes.getRoute('Back Passage')
-        r.activateRoute()
-        r.setRoute()
-        time.sleep(2)
-
-        r = routes.getRoute('Nth Siding 1')
-        r.activateRoute()
-        r.setRoute()
-
-
-        mem = memories.getMemory("North Link Lock")
-        if mem is not None:
-            mem.setValue(None)
-
-        mem = memories.getMemory("South Link Lock")
-        if mem is not None:
-            mem.setValue(None)
-
-        print "StartUp done."
+# class StartUp :
+#
+#     def do(self) :
+#
+#         self.powerState = powermanager.getPower()
+#         if self.powerState != jmri.PowerManager.ON :
+#             powermanager.setPower(jmri.PowerManager.ON)
+#             time.sleep(1)
+#
+#         r = None
+#         tries = 0
+#         while r == None and tries < 10 :
+#             r = routes.getRoute('StartUp')
+#             if r == None:
+#                 tries += 1
+#                 print "tries:", tries
+#                 time.sleep(2)
+#
+#         r.activateRoute()
+#         r.setRoute()
+#         time.sleep(2)
+#
+#         r = routes.getRoute('Hertford Nth Inner')
+#         r.activateRoute()
+#         r.setRoute()
+#         time.sleep(2)
+#
+#         r = routes.getRoute('Sth Sidings 1')
+#         r.activateRoute()
+#         r.setRoute()
+#         time.sleep(2)
+#
+#         r = routes.getRoute('Back Passage')
+#         r.activateRoute()
+#         r.setRoute()
+#         time.sleep(2)
+#
+#         r = routes.getRoute('Nth Siding 1')
+#         r.activateRoute()
+#         r.setRoute()
+#
+#
+#         mem = memories.getMemory("North Link Lock")
+#         if mem is not None:
+#             mem.setValue(None)
+#
+#         mem = memories.getMemory("South Link Lock")
+#         if mem is not None:
+#             mem.setValue(None)
+#
+#         print "StartUp done."
 
 lssensorlist = [112, 113, 114, 115, 116, 117, 105, 106, 107, 108, 109, 110, 101, 102, 103, 119, 122, 123]
 issensorlist = [18, 19, 20, 21, 22, 28, 29, 33, 34, 'IS32']
