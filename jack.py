7c499567783d9dc54a01753790b7875017a3fa54
Steve Rawlinson <steve@tagadab.com>
2017-12-17 15:19:25 +0000

    clear display memories on startup
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- jack.py
+++ jack.py
@@ -33,11 +33,11 @@ from classAnyNth2SthTrack5 import *
 from classAnySth2NthTrack6 import *
 
 # DCC_ADDRESSES = [68, 5144, 2144, 6022, 3213, 1087]
-DCC_ADDRESSES = [2128, 2144, 68, 7405, 1124]
+DCC_ADDRESSES = [2128, 2144, 68, 7405, 1124, 1087, 6719]
 #DCC_ADDRESSES = [5144, 2144]
 #DCC_ADDRESSES = [6719]
 #DCC_ADDRESSES = [7405]
-#DCC_ADDRESSES = [1124, 6719]
+#DCC_ADDRESSES = [1124]
 DEBUG = True
 
 
@@ -424,6 +424,13 @@ class Jack(util.Util, jmri.jmrit.automat.AbstractAutomaton):
         # unselect all sidings
         loco.Loco.unselectSidings(NORTH_SIDINGS + SOUTH_SIDINGS)
         loco.Loco.unselectReverseLoops([NORTH_REVERSE_LOOP, SOUTH_REVERSE_LOOP])
+
+        # clear display memories
+        for i in range(1,6):
+            m = memories.provideMemory("IMTRACK" + str(i) + "LOCO")
+            m.setValue(None)
+            m = memories.provideMemory("IMTRACK" + str(i) + "SPEED")
+            m.setValue(None)
 
         # set sensors to inactive
         for s in ["Add Loco", "Stop", "Estop"]:
