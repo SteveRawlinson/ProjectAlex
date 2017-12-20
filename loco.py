040f2a0a1986c30ec2fe9abb205b76ed49d091db
Steve Rawlinson <steve@tagadab.com>
2017-12-20 19:55:59 +0000

    cater for jno track in setSpeed()
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- loco.py
+++ loco.py
@@ -43,8 +43,9 @@ class Loco(util.Util):
         if type(speed) == str or type(speed) == unicode:
             speed = self.speed(speed)
         self.throttle.setSpeedSetting(speed)
-        mem = memories.provideMemory("IMTRACK" + str(self.track.nr) + "SPEED")
-        mem.setValue(str(round(speed, 2)))
+        if self.track:
+            mem = memories.provideMemory("IMTRACK" + str(self.track.nr) + "SPEED")
+            mem.setValue(str(round(speed, 2)))
         if float(speed) ==  0.0:
             # make sure the message gets through - wait a bit
             time.sleep(0.1)
