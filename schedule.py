86a358dd2921e26e2dfe54a8c3101e23aa9b087d
Steve Rawlinson <steve@tagadab.com>
2017-12-21 20:24:58 +0000

    debug
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- schedule.py
+++ schedule.py
@@ -9,6 +9,7 @@ class HistoricJourney:
         self.startblock = startBlock
         self.endblock = endBlock
         self.average = None
+        self.count = 0
         self.min = None
         self.max = None
         self.nameAndAddress = nameAndAddress
@@ -24,29 +25,34 @@ class HistoricJourney:
 
 
     def calc(self):
-        count = 0
         tot = Decimal(0.0)
         for d in self.durations:
             if self.min is None or d < self.min: self.min = d
             if self.max is None or d > self.max: self.max = d
             tot += d
-            count += 1
-        self.average = tot / count
+            self.count += 1
+        self.average = tot / self.count
 
     def to_s(self):
-        return self.nameAndAddress + ' ' + self.startblock + ' ' + self.endblock + ' ' + str(self.average)
+        return self.nameAndAddress + ' ' + self.startblock + ' ' + self.endblock + ' ' + str(self.average) + ' ' + str(self.count)
 
+
+print "schedule"
 journeys = {}
 
 with open('C:\Users\steve\shortJourney.log', 'r') as fp:
     for line in fp:
         nameAndAddress, startBlock, endBlock, startTime, arriveTime, finishTime, duration = line.split(',')
         j = HistoricJourney(nameAndAddress, startBlock, endBlock)
-        if j.id in journeys:
-            j = journeys[j.id]
+        if j.id() in journeys:
+            j = journeys[j.id()]
+        else:
+            journeys[j.id()] = j
         j.durations.append(Decimal(duration))
 
-for j in journeys:
+print "there are " + str(len(journeys)) + " journeys"
+for k in sorted(journeys.iterkeys()):
+    j = journeys[k]
     j.calc()
     print j.to_s()
 
