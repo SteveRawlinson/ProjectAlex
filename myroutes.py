7dc7341900146ccb14744e80fddddf5caf3f4656
Steve Rawlinson <steve@tagadab.com>
2017-12-31 22:12:13 +0000

    data
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- myroutes.py
+++ myroutes.py
@@ -48,11 +48,11 @@ IRSENSORS = {
 }
 
 ROUTEMAP = {
-            "Sth Sidings 1": ["Sth Sidings 1", "Back Passage"],
-            "Sth Sidings 2": ["Sth Sidings 2", "Back Passage"],
-            "Sth Sidings 3": ["Sth Sidings 3", "Back Passage"],
-            "Sth Sidings 4": ["Sth Sidings 4", "Back Passage"],
-            "Sth Sidings 5": ["Sth Sidings 5", "Back Passage"],
+            "Sth Sidings 1": ["Back Passage", "Sth Sidings 1"],
+            "Sth Sidings 2": ["Back Passage", "Sth Sidings 2"],
+            "Sth Sidings 3": ["Back Passage", "Sth Sidings 3"],
+            "Sth Sidings 4": ["Back Passage", "Sth Sidings 4"],
+            "Sth Sidings 5": ["Back Passage", "Sth Sidings 5"],
             "FP sidings": ["Sth Sidings"],
             "PAL P1": ["Hertford Nth Inner"],
             "PAL P2": ["Hertford Nth Outer"],
@@ -102,12 +102,13 @@ SPEEDMAP = {
     3213: {'fast': 0.3, 'bend': 0.2, 'medium': 0.2, 'slow': 0.1, 'south link to layout': 'fast', 'off track north': 'fast', 'north link to layout': 'fast',
            'south sidings entry': 'slow', 'north sidings entry': 'slow', 'north link to sidings': 0.15},
     # Ave Talgo
-    6719: {'fast': 0.4, 'bend': 'fast', 'medium': 0.25, 'slow': 0.2, 'north sidings exit': 'fast', 'track to north link': 'fast', 'north link to sidings': 0.3, 'north sidings entry': 0.2,
-           'track to south link': 0.4, 'south sidings exit': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'north interlink southbound': 'fast',
+    6719: {'fast': 0.4, 'bend': 'fast', 'medium': 0.25, 'slow': 0.2, 'north sidings exit': 'fast', 'track to north link': 'fast', 'north link to sidings': 0.3,
+           'north sidings entry': 0.2, 'track to south link': 0.4, 'south sidings exit': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'north interlink southbound': 'fast',
            'north link to layout': 'fast'},
     # class 91 virgin
     1124: {'fast': 0.5, 'bend': 0.3, 'medium': 0.35, 'slow': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'south link wait for route': 0.1,
-           'north interlink northbound': 0.5, 'track to south link:': 0.5, 'north sidings entry': 0.1, 'north link to sidings': 'slow'},
+           'north interlink northbound': 0.5, 'track to south link:': 0.5, 'north sidings entry': 0.1, 'north link to sidings': 'slow', 'north interlink southbound': 'fast',
+           'north link to layout': 'fast'},
     # class 47
     "class47": {'fast': 0.6, 'medium': 0.35, 'slow': 0.25, 'north interlink southbound': 'fast'},
     # Underground
@@ -123,7 +124,7 @@ SLOWTIMEMAP = {
     "class150": {"FPK P2": 3, "AAP P3": 1.5, "PAL P2": 2, "North Link": 4, "PAL P1": 4, "AAP P4": 3, "FPK P1": 10, "FPK P4": 2, "AAP P1": 1, "NSG P2": 3,
                  "FPK P3": 8, "AAP P2": 3, "NSG P1": 5},
     # Underground
-    1087: {"FPK P3": 9, "AAP P2": 4, "NSG P1": 8, "PAL P1": 5, "AAP P4": 5, "FPK P1": 11, "FPK P4": 3, "AAP P1": 3, "NSG P2": 1, "FPK P2": 4,
+    1087: {"FPK P3": 9, "AAP P2": 4, "NSG P1": 4, "PAL P1": 5, "AAP P4": 5, "FPK P1": 11, "FPK P4": 3, "AAP P1": 3, "NSG P2": 3, "FPK P2": 4,
            "AAP P3": 5, "PAL P2": 5, "North Link": 4},
     # Javelin
     5004: {'North Link': 5, 'FPK P7': 5, "Nth Fast Outer 1": 5},
