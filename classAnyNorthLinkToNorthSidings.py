9ce76cd09a686790c4db2c1cbd153cbe809cd9a4
Steve Rawlinson <steve@tagadab.com>
2018-01-03 10:05:26 +0000

    unselect the siding after use set loco status
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- classAnyNorthLinkToNorthSidings.py
+++ classAnyNorthLinkToNorthSidings.py
@@ -11,13 +11,16 @@ from myroutes import *
 class ClassAnyNorthLinkToNorthSidings(alex.Alex):
 
     def handle(self):
+        self.loco.status = loco.ACTIVE
         siding = self.loco.selectSiding(NORTH_SIDINGS)
         lok = lock.Lock()
         lok.getLock(NORTH, NORTHBOUND, self.loco)
         if lok.empty():
-            raise RuntimeError("ruh roh")
+            raise RuntimeError("ruh roh - this should be impossible, did this loco release the lock when it stopped?")
         routes = self.requiredRoutes(siding)
         self.shortJourney(False, normalSpeed='fast', slowSpeed='fast', endBlock=siding, stopIRClear=IRSENSORS[siding.getId()], lock=lok, routes=routes)
+        self.loco.status = SIDINGS
+        self.loco.unselectSiding(siding)
 
 class Class150NorthLinkToNorthSidings(ClassAnyNorthLinkToNorthSidings):
     pass
