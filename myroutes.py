2566cd113dcdd72a57b9c3d8cbb0d76e20481958
Steve Rawlinson <steve@tagadab.com>
2018-01-24 12:09:41 +0000

    data
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- myroutes.py
+++ myroutes.py
@@ -104,11 +104,11 @@ SPEEDMAP = {
     3213: {'fast': 0.3, 'bend': 0.2, 'medium': 0.2, 'slow': 0.1, 'south link to layout': 'fast', 'off track north': 'fast', 'north link to layout': 'fast',
            'south sidings entry': 'slow', 'north sidings entry': 'slow', 'north link to sidings': 0.15},
     # TGV
-    4404: {'fast': 0.5, 'bend': 0.3, 'medium': 0.3, 'slow': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'north link to layout': 'fast',
+    4404: {'fast': 0.6, 'bend': 0.3, 'medium': 0.3, 'slow': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'north link to layout': 'fast',
            'south sidings entry': 'slow', 'north sidings entry': 'slow', 'north link to sidings': 0.15},
     # Ave Talgo
-    6719: {'fast': 0.4, 'bend': 'fast', 'medium': 0.25, 'slow': 0.2, 'north sidings exit': 'fast', 'track to north link': 'fast', 'north link to sidings': 0.3,
-           'north sidings entry': 0.2, 'track to south link': 0.4, 'south sidings exit': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'north interlink southbound': 'fast',
+    6719: {'fast': 0.5, 'bend': 'fast', 'medium': 0.35, 'slow': 0.2, 'north sidings exit': 'fast', 'track to north link': 'fast', 'north link to sidings': 0.3,
+           'north sidings entry': 0.3, 'track to south link': 0.5, 'south sidings exit': 0.3, 'south link to layout': 'fast', 'off track north': 'fast', 'north interlink southbound': 'fast',
            'north link to layout': 'fast'},
     # class 91 virgin
     1124: {'fast': 0.5, 'bend': 0.3, 'medium': 0.35, 'slow': 0.2, 'south link to layout': 'fast', 'off track north': 'fast', 'south link wait for route': 0.1,
