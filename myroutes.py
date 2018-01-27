c22a7f6c65465525b8d86f017b2824a9d5caef4d
Steve Rawlinson <steve@tagadab.com>
2018-01-27 16:52:39 +0000

    data
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- myroutes.py
+++ myroutes.py
@@ -127,7 +127,7 @@ SLOWTIMEMAP = {
     # Mallard
     "classA4": {'FPK P7': 13, "NORTH FAST": 15, 'FPK P8': 17, "FPK P4": 5, "NSG P2": 6, "PAL P2": 5 , "FPK P2": 5, "FPK P3": 4},
     # class 150s
-    "class150": {"FPK P2": 3, "AAP P3": 1.5, "PAL P2": 2, "North Link": 4, "PAL P1": 3, "AAP P4": 3, "FPK P1": 10, "FPK P4": 2, "AAP P1": 1, "NSG P2": 3,
+    "class150": {"FPK P2": 3, "AAP P3": 1.5, "PAL P2": 2, "North Link": 4, "PAL P1": 3, "AAP P4": 2, "FPK P1": 10, "FPK P4": 2, "AAP P1": 1, "NSG P2": 3,
                  "FPK P3": 8, "AAP P2": 3, "NSG P1": 4},
     # Underground
     1087: {"FPK P3": 9, "AAP P2": 4, "NSG P1": 2.5, "PAL P1": 5, "AAP P4": 5, "FPK P1": 11, "FPK P4": 3, "AAP P1": 3, "NSG P2": 3, "FPK P2": 4,
