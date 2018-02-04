9977ddde513b9f42ee4fddde1ebdd7a957705a02
Steve Rawlinson <steve@tagadab.com>
2018-02-04 15:38:24 +0000

    new tests
    Signed-off-by: Steve Rawlinson <steve@tagadab.com>

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import jmri
 import java
 from jmri_bindings import *
+from myroutes import *
 
 class Test(jmri.jmrit.automat.AbstractAutomaton):
     def handle(self):
         t = self.getThrottle(68, False)
-        s = t.getSlot()
+        s = t.getLocoNetSlot()
         print "slot", s.getSlot()
         print "id", s.id()
-        print "status", s.getStatus()
+        print "status", s.slotStatus()
+        for k in LN_SLOT_STATUS:
+            print str(k), 'correspond to', str(LN_SLOT_STATUS[k])
+        print 'in-use', LOCO_IN_USE
+        print 'free', LOCO_FREE
+        print 'idle', LOCO_IDLE
+        print 'common', LOCO_COMMON
 
 Test().start()
 
